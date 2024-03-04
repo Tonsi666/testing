521ab4cedc9d9f54e9334b8fad2ea40fe0e103ef
Tonsi666 <toni.strube@docc.techstarter.de>
2024-03-04 14:38:38 +0100

    Testing Aufgaben Tag 1

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// Im Browser/in Javascript: export default ...
+// Wir sind hier aber in NodeJS!
+module.exports.sum = sum;
+module.exports.divide = divide;
+module.exports.multiply = multiply;
+module.exports.subtract = subtract;
---
+++ minMax.js
@@ -0,0 +1,18 @@
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+function max(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+module.exports.min = min;
+module.exports.max = max;
