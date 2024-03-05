6dd0fe39a38767b900b7670fcb760cc0ceeb63ec
Tonsi666 <toni.strube@docc.techstarter.de>
2024-03-05 19:14:03 +0100

    HA 05.03.2024

--- calculator.test.js
+++ calculator.test.js
@@ -1,73 +1,83 @@
 const calculator = require("./calculator");
 
-test("Die Summe von 2 + 5 ist 7", () => {
-  const result = calculator.sum(2, 5);
+describe("Addition", () => {
+  test("Die Summe von 2 + 5 ist 7", () => {
+    const result = calculator.sum(2, 5);
 
-  expect(result).toEqual(7);
+    expect(result).toEqual(7);
+  });
+
+  test('Die Summe von "Apfel" + 5 ist "Apfel5"', () => {
+    const result = calculator.sum("Apfel", 5);
+
+    expect(result).toEqual("Apfel5");
+  });
 });
 
-test('Die Summe von "Apfel" + 5 ist "Apfel5"', () => {
-  const result = calculator.sum("Apfel", 5);
+describe("Division", () => {
+  test("Der Quotient aus 10 durch 2 ist 5", () => {
+    const result = calculator.divide(10, 2);
 
-  expect(result).toEqual("Apfel5");
+    expect(result).toEqual(5);
+  });
+
+  test("Der Quotient aus 10 durch 3 ist nahe an 3,33", () => {
+    const result = calculator.divide(10, 3);
+
+    expect(result).toBeCloseTo(3.33, 2);
+  });
 });
 
-test("Der Quotient aus 10 durch 2 ist 5", () => {
-  const result = calculator.divide(10, 2);
+describe("Division", () => {
+  test("Postive durch negative Zahl ist kleiner als 0", () => {
+    const result = calculator.divide(10, -2);
 
-  expect(result).toEqual(5);
+    expect(result).toBeLessThan(0);
+  });
+
+  test("Negativ durch negative Zahl ist größer als 0", () => {
+    const result = calculator.divide(-10, -2);
+
+    expect(result).toBeGreaterThan(0);
+  });
 });
 
-test("Der Quotient aus 10 durch 3 ist nahe an 3,33", () => {
-  const result = calculator.divide(10, 3);
+describe("Multiplikation", () => {
+  test("Das Produkt aus 2 und 5 ist 10", () => {
+    const result = calculator.multiply(2, 5);
 
-  expect(result).toBeCloseTo(3.33, 2);
+    expect(result).toEqual(10);
+  });
+
+  test("Das Produkt aus -2 und 5 ist -10", () => {
+    const result = calculator.multiply(-2, 5);
+
+    expect(result).toEqual(-10);
+  });
+
+  test("Das Produkt aus 0 und 5 ist 0", () => {
+    const result = calculator.multiply(0, 5);
+
+    expect(result).toEqual(0);
+  });
 });
 
-test("Postive durch negative Zahl ist kleiner als 0", () => {
-  const result = calculator.divide(10, -2);
+describe("Subtraktion", () => {
+  test("Die Differenz von 5 und 2 ist 3", () => {
+    const result = calculator.subtract(5, 2);
 
-  expect(result).toBeLessThan(0);
-});
+    expect(result).toEqual(3);
+  });
 
-test("Negativ durch negative Zahl ist größer als 0", () => {
-  const result = calculator.divide(-10, -2);
+  test("Die Differenz von 5 und -2 ist 7", () => {
+    const result = calculator.subtract(5, -2);
 
-  expect(result).toBeGreaterThan(0);
-});
+    expect(result).toEqual(7);
+  });
 
-test("Das Produkt aus 2 und 5 ist 10", () => {
-  const result = calculator.multiply(2, 5);
+  test("Die Differenz von 5 und 0 ist 5", () => {
+    const result = calculator.subtract(5, 0);
 
-  expect(result).toEqual(10);
-});
-
-test("Das Produkt aus -2 und 5 ist -10", () => {
-  const result = calculator.multiply(-2, 5);
-
-  expect(result).toEqual(-10);
-});
-
-test("Das Produkt aus 0 und 5 ist 0", () => {
-  const result = calculator.multiply(0, 5);
-
-  expect(result).toEqual(0);
-});
-
-test("Die Differenz von 5 und 2 ist 3", () => {
-  const result = calculator.subtract(5, 2);
-
-  expect(result).toEqual(3);
-});
-
-test("Die Differenz von 5 und -2 ist 7", () => {
-  const result = calculator.subtract(5, -2);
-
-  expect(result).toEqual(7);
-});
-
-test("Die Differenz von 5 und 0 ist 5", () => {
-  const result = calculator.subtract(5, 0);
-
-  expect(result).toEqual(5);
+    expect(result).toEqual(5);
+  });
 });
--- minMax.test.js
+++ minMax.test.js
@@ -1,37 +1,41 @@
 const minMax = require("./minMax");
 
-test("Die kleinere Zahl von 7 und 3 ist 3", () => {
-  const result = minMax.min(7, 3);
+describe("Kleinere Zahl ermitteln", () => {
+  test("Die kleinere Zahl von 7 und 3 ist 3", () => {
+    const result = minMax.min(7, 3);
 
-  expect(result).toEqual(3);
+    expect(result).toEqual(3);
+  });
+
+  test("Die kleinere Zahl von -7 und 3 ist -7", () => {
+    const result = minMax.min(-7, 3);
+
+    expect(result).toEqual(-7);
+  });
+
+  test("Die kleinere Zahl von 0 und 3 ist 0", () => {
+    const result = minMax.min(0, 3);
+
+    expect(result).toEqual(0);
+  });
 });
 
-test("Die kleinere Zahl von -7 und 3 ist -7", () => {
-  const result = minMax.min(-7, 3);
+describe("Größere Zahl ermitteln", () => {
+  test("Die größere Zahl von 7 und 3 ist 7", () => {
+    const result = minMax.max(7, 3);
 
-  expect(result).toEqual(-7);
-});
+    expect(result).toEqual(7);
+  });
 
-test("Die kleinere Zahl von 0 und 3 ist 0", () => {
-  const result = minMax.min(0, 3);
+  test("Die größere Zahl von -7 und 3 ist 3", () => {
+    const result = minMax.max(-7, 3);
 
-  expect(result).toEqual(0);
-});
+    expect(result).toEqual(3);
+  });
 
-test("Die größere Zahl von 7 und 3 ist 7", () => {
-  const result = minMax.max(7, 3);
+  test("Die größere Zahl von 0 und 3 ist 3", () => {
+    const result = minMax.max(0, 3);
 
-  expect(result).toEqual(7);
-});
-
-test("Die größere Zahl von -7 und 3 ist 3", () => {
-  const result = minMax.max(-7, 3);
-
-  expect(result).toEqual(3);
-});
-
-test("Die größere Zahl von 0 und 3 ist 3", () => {
-  const result = minMax.max(0, 3);
-
-  expect(result).toEqual(3);
+    expect(result).toEqual(3);
+  });
 });
